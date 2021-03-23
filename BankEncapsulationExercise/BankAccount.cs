6ec4661e75b1103052a3407e55c2e22c1a6e07e1
Gashaye <goshu.anley@dakota-consulting.com>
2021-03-22 21:05:45 -0400

    First Commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    class BankAccount
+    {
+        private double balance = 0;
+        public void Deposit(double myDeposit)
+        {
+            balance+= myDeposit;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var bAccount = new BankAccount();
+            bAccount.Deposit(200.5);
+            Console.WriteLine(bAccount.GetBalance());
+        }
+    }
+}
